854e50345916a73ac7a4706c8b229dffecf6e635
Stredhy <mimoefect@gmail.com>
2025-04-20 06:07:18 -0600

    Añadido de DietStorage view y controller

---
+++ src/main/java/com/javafx/nutrimaker/DietStorageController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.javafx.nutrimaker;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author mimoe
+ */
+public class DietStorageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
