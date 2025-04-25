da037cae52e5574f013bca07d416a495fc32ff80
Uriel <uriel.hernandez8846@alumnos.udg.mx>
2025-04-24 21:39:46 -0600

    Implementacion de metodos del diagrama par LoginController Eventos para botones

--- src/main/java/com/javafx/nutrimaker/LoginController.java
+++ src/main/java/com/javafx/nutrimaker/LoginController.java
@@ -1,15 +1,52 @@
 package com.javafx.nutrimaker;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
     static final String REG_EXP_VER_EMAIL= "[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{5,256}";
     static final String REG_EXP_VER_PASS= "{8,16}";
-    @FXML
 
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    public void login(ActionEvent event) throws IOException {
+        dietStorage(event);
+    }
+
+    @FXML
+    public void signUp(ActionEvent event) throws IOException {
+        dietStorage(event);
+    }
+
+    private boolean isEmpty() {
+        return !passwordTextField.getText().isEmpty() && emailTextField.getText().isEmpty();
+    }
+
+
+    private void dietStorage(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("DietStorage.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Almacenamiento de Dietas");
+        stage.show();
+    }
 
 
 
