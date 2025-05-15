bea05c95a1145f3eac16e84cda169cc8023f5c2a
TheSnitch13 <alan.herrera6610@alumnos.udg.mx>
2025-05-14 23:32:09 -0600

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/javafx/nutrimaker/models/Database.java
+++ src/main/java/com/javafx/nutrimaker/models/Database.java
@@ -5,8 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBase {
-    private static final String walletPath = System.getenv("NUTRIMAKER_WALLET_PATH").replace("\\", "/");
-    private static final String url= "jdbc:oracle:thin:@nutrimaker_medium?TNS_ADMIN=" + walletPath;
+    private static final String url= "jdbc:oracle:thin:@//adb.mx-queretaro-1.oraclecloud.com:1522/nutrimaker_medium.adb.oraclecloud.com";
 
     private static final String username= "NUTRICIONISTA";
     private static final String password= "6m88Zp^t29JG*MXZsY!&";
