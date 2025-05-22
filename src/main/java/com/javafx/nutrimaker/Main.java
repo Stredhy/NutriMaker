9082ffedc9c3e380d83635c29c5901d9a3943452
TheSnitch13 <alan.herrera6610@alumnos.udg.mx>
2025-05-21 23:29:25 -0600

    Too much to explain, but basically the functions on DietRepository work

--- src/main/java/com/javafx/nutrimaker/Main.java
+++ src/main/java/com/javafx/nutrimaker/Main.java
@@ -1,5 +1,7 @@
 package com.javafx.nutrimaker;
 
+import com.javafx.nutrimaker.models.DietSummary;
+import com.javafx.nutrimaker.repository.DietRepository;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,11 +9,12 @@ import javafx.stage.Stage;
 
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginForm.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("DietStorage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Test!");
         stage.setScene(scene);
@@ -20,5 +23,25 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
+
+        DietRepository dietRepository = new DietRepository();
+
+        try {
+            // Obtener la primera página con 10 dietas
+            List<DietSummary> diets = dietRepository.getDiets(0, 10);
+
+            // Imprimir información básica de cada dieta
+            for (DietSummary diet : diets) {
+                System.out.println("ID: " + diet.getDietId());
+                System.out.println("Paciente: " + diet.getPatientName());
+                System.out.println("Peso: " + diet.getWeight());
+                System.out.println("Altura: " + diet.getHeight());
+                System.out.println("Fecha creación: " + diet.getCreationDate());
+                System.out.println("-----------------------------");
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error al obtener dietas: " + e.getMessage());
+        }
     }
 }
